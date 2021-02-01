d3168a75e60f0081aece7ee681b0397456700af8
kota-kaneta <foruri@icloud.com>
2021-02-01 16:26:56 +0900

    gsap導入

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+
 var VueScrollTo = require('vue-scrollto');
 Vue.use(VueScrollTo)
 
 Vue.config.productionTip = false
+
+import { gsap } from "gsap";
+import { PixiPlugin } from "gsap/PixiPlugin.js";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin.js";
+gsap.registerPlugin(PixiPlugin, MotionPathPlugin);
 
 new Vue({
   router,
